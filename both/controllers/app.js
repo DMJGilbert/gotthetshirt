cc7c31b3bc2a08ca7400586d2bd93555429770ac
Max Maybury <max.maybury@techex.co.uk>
2015-02-24 14:32:35 +0000

    Menu button on the app page.

--- both/controllers/app.js
+++ both/controllers/app.js
@@ -3,3 +3,13 @@ AppController = RootController.extend({
 
   }
 });
+
+AppController.events({
+    "click #menuBtn": function (e, tmpl) {
+		if(window.location.pathname == '/'){
+			window.location.href = '/app';
+		}
+        var menu = document.getElementById('menu');
+		menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+    }
+});
